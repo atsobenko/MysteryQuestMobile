4f335c4198e40332deb758b544f42ed679f4175b
suchgo <ikruasan@yandex.ru>
2020-01-02 03:11:21 +0200

    Add decor tilemap. Add candle light

---
+++ Assets/Scripts/Lights/Candle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class Candle : MonoBehaviour
+{
+    private Light2D _light;
+    private float _timer;
+    public float delay, minValue, maxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light2D>();
+        _timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - _timer >= delay)
+        {
+            _timer = Time.time;
+            _light.intensity = Random.Range(minValue, maxValue);
+        }
+    }
+}
