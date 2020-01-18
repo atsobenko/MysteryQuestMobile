97b08dd177cff1215058d318fc9acc03867df7a9
suchgo <ikruasan@yandex.ru>
2020-01-18 20:00:34 +0200

    Add pickable highlight effect

---
+++ Assets/Scripts/Objects/Item/HighlightItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HighlightItem : MonoBehaviour
+{
+    public Material standartMaterial;
+    public Material highlightMaterial;
+    public Renderer componentRenderer;
+
+    private void OnTriggerEnter2D(Collider2D inputCollider)
+    {
+        SetMaterial(highlightMaterial, inputCollider);
+    }
+
+    private void OnTriggerExit2D(Collider2D inputCollider)
+    {
+        SetMaterial(standartMaterial, inputCollider);
+    }
+
+    private void SetMaterial(Material material, Collider2D inputCollider)
+    {
+
+        if (!inputCollider.CompareTag("Player") || !componentRenderer)
+        {
+            return;
+        }
+
+        componentRenderer.material = material;
+    }
+}
